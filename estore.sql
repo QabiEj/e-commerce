fd16d6c57a1e18d8a626bb372c58e30f77b3841c
ShabanEjupi <101940223+ShabanEjupi@users.noreply.github.com>
2023-05-26 22:06:54 +0200

    Update estore.sql

--- estore.sql
+++ estore.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2023 at 09:19 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: May 26, 2023 at 10:06 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 5.6.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `categories`
@@ -39,7 +40,8 @@ CREATE TABLE `categories` (
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'Food'),
 (2, 'Drinks'),
-(3, 'Pasta');
+(3, 'Pasta'),
+(7, 'Ushqimet');
 
 -- --------------------------------------------------------
 
@@ -51,9 +53,9 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `customer_data` text NOT NULL,
-  `notes` text DEFAULT NULL,
+  `notes` text,
   `total` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `orders`
@@ -73,7 +75,7 @@ INSERT INTO `orders` (`id`, `user_id`, `customer_data`, `notes`, `total`) VALUES
 CREATE TABLE `order_product` (
   `order_id` int(11) DEFAULT NULL,
   `products_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `order_product`
@@ -97,11 +99,11 @@ CREATE TABLE `products` (
   `name` varchar(100) DEFAULT NULL,
   `price` float DEFAULT NULL,
   `qty` int(11) DEFAULT NULL,
-  `discount` int(11) DEFAULT 0,
-  `description` text DEFAULT NULL,
+  `discount` int(11) DEFAULT '0',
+  `description` text,
   `image` varchar(150) DEFAULT 'noimage.png',
   `category_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products`
@@ -111,7 +113,8 @@ INSERT INTO `products` (`id`, `user_id`, `name`, `price`, `qty`, `discount`, `de
 (2, 1, 'Bread', 0.45, 10, 5, 'White Bread 500gr', '1678134163download.jpg', 1),
 (6, 1, 'Coca Cola 0.5l', 1.25, 10, 0, 'Coca Cola 0.5l\r\n<br />\r\nOrigin: RKS', '16782999921678132836coca-cola-original-20oz.png', 2),
 (7, 1, 'Coca Cola 0.33l', 0.88, 50, 2, 'Coca Cola 0.33l\r\n<br />\r\nOrigin: Macedonia', '16782999721678133665coca-cola-classic-033-l-sweet-water-w-w.jpg', 2),
-(8, 1, 'Fanta 0.5l', 1.15, 8, 10, 'Fanta 0.5l\r\n<br />\r\nOrigin: RKS', '1678302734fanta-0.5-L-pet.jpg', 2);
+(8, 1, 'Fanta 0.5l', 1.15, 8, 10, 'Fanta 0.5l\r\n<br />\r\nOrigin: RKS', '1678302734fanta-0.5-L-pet.jpg', 2),
+(14, 2, 'Chipsa Vipa', 0.5, 100, 5, '', '168513094308-Clasic-i-thjesht.png', 1);
 
 -- --------------------------------------------------------
 
@@ -123,9 +126,9 @@ CREATE TABLE `promotions` (
   `id` int(11) NOT NULL,
   `title` varchar(100) DEFAULT NULL,
   `subtitle` varchar(100) DEFAULT NULL,
-  `is_active` tinyint(1) DEFAULT 1,
+  `is_active` tinyint(1) DEFAULT '1',
   `image` varchar(255) NOT NULL DEFAULT 'noimage.png'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `promotions`
@@ -152,15 +155,16 @@ CREATE TABLE `users` (
   `phone` varchar(45) DEFAULT NULL,
   `avatar` varchar(100) DEFAULT 'avatar.png',
   `role` varchar(45) DEFAULT 'customer'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `address`, `phone`, `avatar`, `role`) VALUES
-(1, 'Artan', 'Gashi', 'artani@gmail.com', 'asdfsfsdfdsfsd', 'cdsxfvsddfds', 'r5tr34wr53434', 'avatar.png', '1'),
-(2, 'Ylber', 'Veliu', 'ylber.veliu@yahoo.com', '$2y$10$l7c.B4WAtLAkhl6sClRu5.4yccvXoW2CiEksWTH2LMYdjqj95FvjO', 'Po Box 105', '070580780', '16787383093.jpg', 'admin');
+(1, 'Arianit', 'Likaj', 'arianitlikaj46@gmail.com', 'asdfsfsdfdsfsd', 'cdsxfvsddfds', 'r5tr34wr53434', 'avatar.png', '1'),
+(2, 'Flamur', 'Avdylaj', 'flamur.avdylaj1@gmail.com', '$2y$10$l7c.B4WAtLAkhl6sClRu5.4yccvXoW2CiEksWTH2LMYdjqj95FvjO', 'Po Box 105', '070580780', '16787383093.jpg', 'admin'),
+(3, 'Shaban', 'Ejupi', 'shabanejupi5@gmail.com', '$2y$10$0POZWcE1ZLYxJfFkMW34CucT6BZtcbDeQ.QkDPtG8iwZeFYDa6pR2', NULL, NULL, 'avatar.png', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -214,7 +218,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -226,19 +230,19 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `promotions`
 --
 ALTER TABLE `promotions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
